89e44e3d8453b62498802c7a866fb8f91b141677
MateoSaettone <mattsmadzyt@gmail.com>
2024-11-02 13:17:40 -0400

    initial commit

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+# Activate venv - mac source ./venv/bin/activate
+# Activate fastAPI - uvicorn main:app --reload
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Backend for Accessibility App"}
